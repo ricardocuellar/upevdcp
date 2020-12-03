9dbaf4d670f47a1e3f960f3857d67e6b0df43689
Ricardo Cuellar <rcuellarsn@gmail.com>
2020-12-03 01:43:01 -0600

    Install adn add Mysql environment to the project

--- upevdcp/settings.py
+++ upevdcp/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'upevdcp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'upevdcp',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
 }
 
 
